5d0b2ac9531552348974232dfd7829875e18b0f5
functional-cron-8324 <nicholaszafiriou@gmail.com>
2020-02-10 17:08:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,13 +1,14 @@
 def mario
-  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
-$status = "Thank You Mario! But Our Princess Is In Another Castle!"
+status = "Thank You Mario! But Our Princess Is In Another Castle!"
 
 
 def toadstool
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
 
